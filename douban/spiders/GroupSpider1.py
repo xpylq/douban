eeccb3900c1d405c57a662b096e8481390b7a1b8
youzhihao <380104026@qq.com>
2017-09-29 11:50:46 +0800

    提交配置文件

--- douban/spiders/GroupSpider1.py
+++ douban/spiders/GroupSpider1.py
@@ -11,7 +11,7 @@ class GroupSpider(scrapy.Spider):
     name = "groupSpider1"
 
     def start_requests(self):
-        urls = ['https://www.douban.com/j/search?q=' + self.search + '单身+&start=0&cat=1019']
+        urls = ['https://www.douban.com/j/search?q=' + self.search + '&start=0&cat=1019']
         print "urls", urls
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
